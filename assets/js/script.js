d02d6e8505f32f0e5e7acd829506229f39313470
Javeriya Saeed Abbasi <jiyasaeed333@gmail.com>
2023-02-07 16:53:14 +0500

    cube-portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -358,117 +358,133 @@ jQuery(function ($) {
    //    }
    // });
 
+   
+//Company Stats Number Counter:
+$(window).scroll(testScroll);
+   var viewed = false;
+   
+   function isScrolledIntoView(elem) {
+       var docViewTop = $(window).scrollTop();
+       var docViewBottom = docViewTop + $(window).height();
+   
+       var elemTop = $(elem).offset().top;
+       var elemBottom = elemTop + $(elem).height();
+   
+       return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+   }
+   function testScroll() {
+      if (isScrolledIntoView($(".company-stats-section")) && !viewed) {
+          viewed = true;
+          $('.stats-number-inner span').each(function () {
+          $(this).prop('Counter',0).animate({
+              Counter: $(this).text()
+          }, {
+              duration: 3000,
+              easing: 'swing',
+              step: function (now) {
+                  $(this).text(Math.ceil(now));
+              }
+          });
+        });
+      }
+    }
 
-   // Number Counter Which Will Run On Appearing On The Screen
-   // $(".company-stats-section").appear(function () {
-   //    $('.stats-number-inner span').each(function () {
-   //       $(this).prop('Counter', 0).animate({
-   //          Counter: $(this).text()
-   //       }, {
-   //          duration: 3000,
-   //          easing: 'swing',
-   //          step: function (now) {
-   //             $(this).text(Math.ceil(now));
-   //          }
-   //       });
-   //    });
-   // });
 
 
    // Cube Portfolio Initializing
-   // $('#js-grid-mosaic').cubeportfolio({
-   //       filters: '#js-filters-mosaic',
-   //       layoutMode: 'grid',
-   //       sortByDimension: true,
-   //       mediaQueries: [{
-   //          width: 1500,
-   //          cols: 2,
-   //      }, {
-   //          width: 1100,
-   //          cols: 2,
-   //      }, {
-   //          width: 768,
-   //          cols: 1,
-   //      }, {
-   //          width: 480,
-   //          cols: 1,
-   //          options: {
-   //             gapHorizontal: 60
-   //          }
-   //      }],
-   //       defaultFilter: '*',
-   //       animationType: 'fadeOut',
-   //       gapHorizontal: 120,
-   //       gapVertical: 150,
-   //       gridAdjustment: 'responsive',
-   //       caption: 'zoom',
+   $('#js-grid-mosaic').cubeportfolio({
+         filters: '#js-filters-mosaic',
+         layoutMode: 'grid',
+         sortByDimension: true,
+         mediaQueries: [{
+            width: 1500,
+            cols: 2,
+        }, {
+            width: 1100,
+            cols: 2,
+        }, {
+            width: 768,
+            cols: 1,
+        }, {
+            width: 480,
+            cols: 1,
+            options: {
+               gapHorizontal: 60
+            }
+        }],
+         defaultFilter: '*',
+         animationType: 'fadeOut',
+         gapHorizontal: 120,
+         gapVertical: 150,
+         gridAdjustment: 'responsive',
+         caption: 'zoom',
 
-   //       // lightbox
-   //       lightboxDelegate: '.cbp-lightbox',
-   //       lightboxGallery: true,
-   //       lightboxTitleSrc: 'data-title',
-   //       lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
+         // lightbox
+         lightboxDelegate: '.cbp-lightbox',
+         lightboxGallery: true,
+         lightboxTitleSrc: 'data-title',
+         lightboxCounter: '<div class="cbp-popup-lightbox-counter">{{current}} of {{total}}</div>',
 
-   //       plugins: {
-   //          loadMore: {
-   //             element: "#js-loadMore-lightbox-gallery",
-   //             action: "click",
-   //             loadItems: 5,
-   //          }
-   //       }
+         plugins: {
+            loadMore: {
+               element: "#js-loadMore-lightbox-gallery",
+               action: "click",
+               loadItems: 5,
+            }
+         }
 
-   //    })
-   //    .on('initComplete.cbp', function () {
-   //       // your functionality
-   //       var $this = $(this);
-   //       if ($(".cbp-filter-item-active").attr("data-filter") === "*") {
-   //          $("#js-loadMore-lightbox-gallery").addClass("active");
-   //       } else {
-   //          $("#js-loadMore-lightbox-gallery").removeClass("active");
-   //       }
-   //       $this.find(".cbp-wrapper").find(".cbp-item:not(.cbp-item-off)").each(function (index) {
-   //          $(this).removeClass("even");
+      })
+      .on('initComplete.cbp', function () {
+         // your functionality
+         var $this = $(this);
+         if ($(".cbp-filter-item-active").attr("data-filter") === "*") {
+            $("#js-loadMore-lightbox-gallery").addClass("active");
+         } else {
+            $("#js-loadMore-lightbox-gallery").removeClass("active");
+         }
+         $this.find(".cbp-wrapper").find(".cbp-item:not(.cbp-item-off)").each(function (index) {
+            $(this).removeClass("even");
 
-   //          console.log();
-   //          var val = index + 1;
-   //          if ($(this).css('left') !== "0px") {
-   //             $(this).addClass("even");
+            console.log();
+            var val = index + 1;
+            if ($(this).css('left') !== "0px") {
+               $(this).addClass("even");
 
-   //          }
-   //       });
-   //    })
-   //    .on('onAfterLoadMore.cbp', function () {
-   //       // your functionality
-   //       var $this = $(this);
-   //       $("#js-loadMore-lightbox-gallery a").addClass("d-none");
-   //       $("#js-loadMore-lightbox-gallery").addClass("active-outer");
-   //       $this.find(".cbp-wrapper").find(".cbp-item:not(.cbp-item-off)").each(function (index) {
-   //          $(this).removeClass("even");
-   //          console.log();
-   //          var val = index + 1;
-   //          if ($(this).css('left') !== "0px") {
-   //             $(this).addClass("even");
-   //          }
-   //       });
-   //    })
-   //    .on('filterComplete.cbp', function () {
-   //       // your functionality
-   //       var $this = $(this);
-   //       if ($(".cbp-filter-item-active").attr("data-filter") === "*") {
-   //          $("#js-loadMore-lightbox-gallery").addClass("active");
-   //          $("#js-loadMore-lightbox-gallery").removeClass("d-none");
-   //       } else {
-   //          $("#js-loadMore-lightbox-gallery").removeClass("active");
-   //          $("#js-loadMore-lightbox-gallery").addClass("d-none");
-   //       }
-   //       $this.find(".cbp-wrapper").find(".cbp-item:not(.cbp-item-off)").each(function (index) {
-   //          $(this).removeClass("even");
-   //          var val = index + 1;
-   //          if ($(this).css('left') !== "0px") {
-   //             $(this).addClass("even");
-   //          }
-   //       });
-   //    });
+            }
+         });
+      })
+      .on('onAfterLoadMore.cbp', function () {
+         // your functionality
+         var $this = $(this);
+         $("#js-loadMore-lightbox-gallery a").addClass("d-none");
+         $("#js-loadMore-lightbox-gallery").addClass("active-outer");
+         $this.find(".cbp-wrapper").find(".cbp-item:not(.cbp-item-off)").each(function (index) {
+            $(this).removeClass("even");
+            console.log();
+            var val = index + 1;
+            if ($(this).css('left') !== "0px") {
+               $(this).addClass("even");
+            }
+         });
+      })
+      .on('filterComplete.cbp', function () {
+         // your functionality
+         var $this = $(this);
+         if ($(".cbp-filter-item-active").attr("data-filter") === "*") {
+            $("#js-loadMore-lightbox-gallery").addClass("active");
+            $("#js-loadMore-lightbox-gallery").removeClass("d-none");
+         } else {
+            $("#js-loadMore-lightbox-gallery").removeClass("active");
+            $("#js-loadMore-lightbox-gallery").addClass("d-none");
+         }
+         $this.find(".cbp-wrapper").find(".cbp-item:not(.cbp-item-off)").each(function (index) {
+            $(this).removeClass("even");
+            var val = index + 1;
+            if ($(this).css('left') !== "0px") {
+               $(this).addClass("even");
+            }
+         });
+      });
    
    
    /*---- Wow Initializing ----*/
@@ -637,6 +653,8 @@ var swiper = new Swiper(".mySwiper2", {
      },
    },
  });
+
+
  // Team Slider Single image on left side
 //  if ($(".team-section").hasClass("team-section-two")) {
 //    new Swiper(".team-member-slider-2", {
